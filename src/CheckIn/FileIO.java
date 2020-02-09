6b59fa5a99e445a4060298868d49f2b1ac12ee12
jamie <jamiefhill@gmail.com>
2020-02-09 15:29:58 +0000

    Tidied up imports

--- src/CheckIn/FileIO.java
+++ src/CheckIn/FileIO.java
@@ -1,11 +1,9 @@
 package CheckIn;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-// import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 public class FileIO {
