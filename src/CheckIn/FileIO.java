5088a4bfa2543844911fbd7eb341cc5641e20c8b
jamie <jamiefhill@gmail.com>
2020-02-09 15:53:12 +0000

    Added writeFile method with tests

--- src/CheckIn/FileIO.java
+++ src/CheckIn/FileIO.java
@@ -1,10 +1,13 @@
 package CheckIn;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class FileIO {
 
@@ -15,6 +18,7 @@ public class FileIO {
 		    while ((line = buffReader.readLine()) != null) {
 		    	fileContents.add(line);
 		    }
+			buffReader.close();
 		}
 		return fileContents;
 	}
@@ -22,4 +26,21 @@ public class FileIO {
 	// TODO
 	// Check line is text characters only and not binary
 
+	public void writeFile(String fileName, ArrayList<String> fileContents) throws IOException {
+		if(fileContents.size() > 0) {
+	        File file = new File(fileName);
+	        FileWriter writer = null;
+	        try {
+	            writer = new FileWriter(file);
+	            Iterator<String> fileContentsIt = fileContents.iterator();
+	            while(fileContentsIt.hasNext()) {
+	            	writer.write(fileContentsIt.next() + "\n");
+	            }
+	        } finally {
+	        	writer.close();
+	        }
+		} else {
+			throw new IOException("Empty file contents detected");
+		}
+	}
 }
--- tests/FileIOTests.java
+++ tests/FileIOTests.java
@@ -2,6 +2,7 @@
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,6 +15,11 @@ public class FileIOTests {
 
 	private String validAsciiFile = "bookings.csv";
 	private String invalidAsciiFile = "noBookings.csv";
+	private String testWriteFile = "test.csv";
+	private String lineOne = "Line One";
+	private String lineTwo = "Line Two";
+	private String lineThree = "Line Three";
+	private int contentTestSize = 3;
 	private int contentLineSize = 5;
 	
 	@Test
@@ -28,5 +34,28 @@ public class FileIOTests {
 		FileIO FileRead = new FileIO();
 		FileRead.readFile(invalidAsciiFile);
 	}
+	
+	@Test
+	public void testWriteFileSuccess() throws IOException {
+		FileIO FileWrite = new FileIO();
+		ArrayList<String> fileContent = new ArrayList<String>();
+		fileContent.add(lineOne);
+		fileContent.add(lineTwo);
+		fileContent.add(lineThree);
+		FileWrite.writeFile(testWriteFile, fileContent);
+		ArrayList<String> readContents = FileWrite.readFile(testWriteFile);
+		assertEquals(contentTestSize, readContents.size());
+		File file = new File(testWriteFile); 
+		file.delete();
+	}
+	
+	@Test(expected = IOException.class)
+	public void testWriteFileNoContent() throws IOException {
+		FileIO FileWrite = new FileIO();
+		ArrayList<String> fileContent = new ArrayList<String>();
+		FileWrite.writeFile(testWriteFile, fileContent);
+	}
 
+	// TODO
+	// Test for IOException when overwriting a read-only file?
 }
