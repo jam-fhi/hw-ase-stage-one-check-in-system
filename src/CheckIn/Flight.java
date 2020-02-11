b845a9b3cf8827de6439c3e87d606e4ef5617394
Nadia Abulhawa <na101@hw.ac.uk>
2020-02-11 10:07:31 +0000

    Added flightCode

---
+++ src/CheckIn/Flight.java
@@ -0,0 +1,14 @@
+package CheckIn;
+
+public class Flight {
+	private String flightCode;
+
+	public Flight(String flightCode) {
+		this.flightCode = flightCode;
+	}
+
+	public String getFlightCode() {
+		return flightCode;
+	}
+
+}
