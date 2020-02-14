13a6f2ad1dc32ac37d4707c5ad752ce7bb2f834e
Nadia Abulhawa <na101@hw.ac.uk>
2020-02-14 16:06:01 +0000

    Flight class and flight tests

--- src/CheckIn/Flight.java
+++ src/CheckIn/Flight.java
@@ -2,13 +2,59 @@ package CheckIn;
 
 public class Flight {
 	private String flightCode;
+	private String destinationAirport;
+	private String carrier;
+	private int maximumPassengers;
+	private double maximumBaggageWeight;
+	private double maximumBaggageVolume;
+	private double excessCharge;
 
-	public Flight(String flightCode) {
+	public Flight(String flightCode, String destinationAirport, String carrier, int maximumPassengers,
+			double maximumBaggageWeight, double maximumBaggageVolume, double excessCharge) {
+
 		this.flightCode = flightCode;
+		this.destinationAirport = destinationAirport;
+		this.carrier = carrier;
+		this.maximumPassengers = maximumPassengers;
+		this.maximumBaggageWeight = maximumBaggageWeight;
+		this.maximumBaggageVolume = maximumBaggageVolume;
+		this.excessCharge = excessCharge;
 	}
 
 	public String getFlightCode() {
 		return flightCode;
+	}
+
+	public String getDestinationAirport() {
+		return destinationAirport;
+	}
+
+	public String getCarrier() {
+		return carrier;
+	}
+
+	public int getMaximumPassengers() {
+		return maximumPassengers;
+	}
+
+	public double getMaximumBaggageWeight() {
+		return maximumBaggageWeight;
+	}
+
+	public double getMaximumBaggageVolume() {
+		return maximumBaggageVolume;
+	}
+
+	public double getExcessCharge() {
+		return excessCharge;
+	}
+
+	public double getAllowedBaggageWeightPerPassenger() {
+		return maximumBaggageWeight / maximumPassengers;
+	}
+
+	public double getAllowedBaggageVolumePerPassenger() {
+		return maximumBaggageVolume / maximumPassengers;
 	}
 
 }
