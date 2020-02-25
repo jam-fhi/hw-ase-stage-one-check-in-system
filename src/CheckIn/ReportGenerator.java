681405a1928c0389ff873d8cf8ee4dd8aac05de6
jamie <jamiefhill@gmail.com>
2020-02-25 22:31:33 +0000

    Adds comments to the report generator

--- src/CheckIn/ReportGenerator.java
+++ src/CheckIn/ReportGenerator.java
@@ -5,87 +5,135 @@ import java.util.Iterator;
 
 public class ReportGenerator {
 	
+	// Flight and booking collection variables
 	private BookingCollection bookings;
 	private FlightCollection flights;
 	
 	public ReportGenerator(BookingCollection bookings, FlightCollection flights, String reportFileName) throws CheckInIOException {
+		// Store the booking and flights collections in class variables
 		this.bookings = bookings;
 		this.flights = flights;
+		// Generate the report with the specified file name
 		generateReport(reportFileName);
 	}
 	
 	private void generateReport(String reportFileName) throws CheckInIOException {
+		// Create an iterator to loop through all the flights
 		Iterator<Flight> flightsIt = flights.getFlightCollection().iterator();
+		// Create an array list of string arrays to store the csv report output
 		ArrayList<String[]> flightReport = new ArrayList<String[]>();
+		// Loop through all the flights
 		while(flightsIt.hasNext()) {
+			// Get the next flight
 			Flight aFlight = flightsIt.next();
+			// Find all bookins for this flight
 			ArrayList<Booking> flightBookings = bookings.getBookingByFlightCode(aFlight.getFlightCode());
+			// Temporary storage for one line of the report
 			String[] flightReportLine = new String[5];
+			// If there are bookins then we want to process them
 			if(flightBookings.size() > 0) {
+				// Get the number of passengers who are checked into a flight
 				int passengersCheckedIn = getPassengersCheckedIn(flightBookings);
+				// Get the total baggage weight from the passengers who are checked in to the flight
 				double totalBaggageWeight = getPassengersTotalBaggageWeight(flightBookings);
+				// Get the total baggage volume from the passengers who are checked into the flight
 				double totalBaggageVolume = getPassengersTotalBaggageVolume(flightBookings);
+				// Get the total excess baggage weight charge from the passengers who are checked into the flight
 				double totalExcessCharge = getPassengersTotalExcessCharge(flightBookings);
+				// Get the flights remaining capacity by subtracting the checked in passenger count from the maximum passengers allowed
 				int flightCapacityRemaining = aFlight.getMaximumPassengers() - passengersCheckedIn;
+				// Added the generated report data to the temporary line string array
 				flightReportLine[0] = String.valueOf(passengersCheckedIn);
 				flightReportLine[1] = String.valueOf(totalBaggageWeight);
 				flightReportLine[2] = String.valueOf(totalBaggageVolume);
 				flightReportLine[3] = String.valueOf(totalExcessCharge);
 				flightReportLine[4] = String.valueOf(flightCapacityRemaining);
+				// Add the line to the flight report.
 				flightReport.add(flightReportLine);
 			}
 		}
+		// If there are entries in the flight report, write it to file
 		if(flightReport.size() > 0) {
+			// Create a new csv processor object
 			CSVProcessor csvProc = new CSVProcessor();
+			// Give it the report file name and the flight report contents to write it to file
 			csvProc.parseStringArrayToCSV(reportFileName, flightReport);
 		}
 	}
 	
 	private int getPassengersCheckedIn(ArrayList<Booking> flightBookings) {
+		// Set the initial checked in passenger count to 0
 		int checkedInCount = 0;
+		// Create an iterator to process the bookings
 		Iterator<Booking> flightBookingsIt = flightBookings.iterator();
+		// Loop through the bookings
 		while(flightBookingsIt.hasNext()) {
+			// Extract the passenger from the booking
 			Passenger traveller = flightBookingsIt.next().getPassenger();
+			// If the passenger is checked in
 			if(traveller.isCheckIn()) {
+				// Increment the passenger checked in count
 				checkedInCount++;
 			}
 		}
+		// Return 
 		return checkedInCount;
 	}
 	
 	private double getPassengersTotalBaggageWeight(ArrayList<Booking> flightBookings) {
+		// Create a total baggage weight variable and initialise it to 0
 		double totalBaggageWeight = 0;
+		// Create an iterator to process all the bookings
 		Iterator<Booking> flightBookingsIt = flightBookings.iterator();
+		// Loop through the bookings while there is a next booking
 		while(flightBookingsIt.hasNext()) {
+			// Get the booking to process
 			Booking aBooking = flightBookingsIt.next();
+			// If the passenger is checked in
 			if(aBooking.getPassenger().isCheckIn()) {
+				// Add the baggage weight to the total baggage weight count
 				totalBaggageWeight += aBooking.getPassenger().getBaggage().getWeight();
 			}
 		}
+		// Return the total baggage weight
 		return totalBaggageWeight;
 	}
 	
 	private double getPassengersTotalBaggageVolume(ArrayList<Booking> flightBookings) {
+		// Create a total baggage volume variable and initialise it to 0
 		double totalBaggageVolume = 0;
+		// Create an iterator to loop through all the bookings
 		Iterator<Booking> flightBookingsIt = flightBookings.iterator();
+		// Loop through the bookings while there is a next booking
 		while(flightBookingsIt.hasNext()) {
+			// Get the next booking object
 			Booking aBooking = flightBookingsIt.next();
+			// If the passenger is checked in to the flight
 			if(aBooking.getPassenger().isCheckIn()) {
+				// Add the passengers baggage volume to the total
 				totalBaggageVolume += aBooking.getPassenger().getBaggage().getVolume();
 			}
 		}
+		// Return the total baggage volume
 		return totalBaggageVolume;
 	}
 	
 	private double getPassengersTotalExcessCharge(ArrayList<Booking> flightBookings) {
+		// Create a total excess charge variable and initialise it to 0
 		double totalExcessCharge = 0;
+		// Get an iterator to loop through all the bookings
 		Iterator<Booking> flightBookingsIt = flightBookings.iterator();
+		// While there is a next flight booking
 		while(flightBookingsIt.hasNext()) {
+			// Get the next flight booking
 			Booking aBooking = flightBookingsIt.next();
+			// If the passenger is checked into the flight
 			if(aBooking.getPassenger().isCheckIn()) {
+				// Add the passengers excess baggage charge to the total
 				totalExcessCharge += aBooking.getPassenger().getBaggage().getExcessCharge();
 			}
 		}
+		// Return the total excess charge for the flight
 		return totalExcessCharge;
 	}
 }
--- tests/ReportGeneratorTest.java
+++ tests/ReportGeneratorTest.java
@@ -13,48 +13,77 @@ import CheckIn.ReportGenerator;
 
 public class ReportGeneratorTest {
 
+	// Test files to test with
 	private String reportFile = "FlightsReportTest.csv";
 	private String bookingFile = "bookings.csv";
 	private String flightFile = "flights.csv";
+	// Valid baggage weight test variable
 	private double validAllowedBaggageWeight = 10.0;
+	// Valid excess charge test variable
 	private double validExcessCharge = 10;
+	// Expected number of lines in the flight report
 	private int expectedFlightReportLines = 3;
+	// Valid width, length, height and weight to test with
 	private int validWidth = 15;
 	private int validLength = 15;
 	private int validHeight = 15;
 	private double validWeight = 15.0;
+	// Expected number of passengers checked in
 	private int expectedCheckedIn = 1;
+	// Expected Weight, volume test variables
 	private double expectedWeight = 15.0;
 	private double expectedVolume = 3375.0;
+	// Expected excess charge amount
 	private double expectedExcessCharge = 50.0;
+	// Expected remaining capacity test variable
 	private double expectedRemainingCapacity = 122;
+	// Delta Precision Loss, required for asserting double types
 	private double deltaPrecisionLoss = 0.01;
+	// First report index in the array list of read in report data
 	private int firstReport = 0;
 	
 	private Bag aBag = new Bag(validWidth, validLength, validHeight, validWeight);
 	
 	@Test
 	public void testReportGenerator() throws BookingException, CheckInIOException {
+		// Create a new booking collection and load bookings
 		BookingCollection Bookings = new BookingCollection(bookingFile);
+		// Add a bag to one passenger
 		Bookings.getBooking("BA123-121", "Hill").getPassenger().addBaggage(aBag);
+		// Set the passenger as checked in
 		Bookings.getBooking("BA123-121", "Hill").getPassenger().setCheckIn();
+		// Calculate the excess charge
 		Bookings.getBooking("BA123-121", "Hill").getPassenger().getBaggage().setExcessCharge(validAllowedBaggageWeight, validExcessCharge);
+		// Add bags to the other passengers
 		Bookings.getBooking("BA124-122", "Abulhawa").getPassenger().addBaggage(aBag);
 		Bookings.getBooking("BA123-123", "Ghoghari").getPassenger().addBaggage(aBag);
 		Bookings.getBooking("BA124-124", "McFarland").getPassenger().addBaggage(aBag);
 		Bookings.getBooking("BA125-125", "Muir").getPassenger().addBaggage(aBag);
+		// Create a flight collection from the flight collection file
 		FlightCollection Flights = new FlightCollection(flightFile);
+		// Create a report generator object, taking the test data to output in a report file
 		new ReportGenerator(Bookings, Flights, reportFile);
+		// Create a new csv processor object
 		CSVProcessor csvProc = new CSVProcessor();
+		// Read in the report that was generated
 		ArrayList<String[]> report = csvProc.parseCSVToStringArray(reportFile);
+		// Get a file handle for the test report file
 		File file = new File(reportFile); 
+		// Delete the test report file as part of test clean up
 		file.delete();
+		// Assert the expected number of lines in the test report file
 		assertEquals(expectedFlightReportLines, report.size());
+		// Extract the first entry in the test report file
 		String[] reportLine = report.remove(firstReport);
+		// Assert that the number of passengers checked in is as expected
 		assertEquals(expectedCheckedIn, Integer.parseInt(reportLine[0]));
+		// Assert the weight checked in is as expected
 		assertEquals(expectedWeight, Double.parseDouble(reportLine[1]), deltaPrecisionLoss);
+		// Assert the volume checked in is as expected
 		assertEquals(expectedVolume, Double.parseDouble(reportLine[2]), deltaPrecisionLoss);
+		// Assert the excess charge checked in is as expected
 		assertEquals(expectedExcessCharge, Double.parseDouble(reportLine[3]), deltaPrecisionLoss);
+		// Assert that the remaining capacity checked in is as expected
 		assertEquals(expectedRemainingCapacity, Integer.parseInt(reportLine[4]), deltaPrecisionLoss);
 	}
 }
