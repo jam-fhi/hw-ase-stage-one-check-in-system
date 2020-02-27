12ff455066dac3f4810d6cf8f58a046c1fc106bc
Haikah <39999526+Haikah@users.noreply.github.com>
2020-02-27 09:26:52 +0000

    Commented passenger and bag class (#41)

--- src/CheckIn/Bag.java
+++ src/CheckIn/Bag.java
@@ -1,5 +1,10 @@
 package CheckIn;
 
+/**
+ * Bag creating width, length, height, weight, and excessCharge
+ * 
+ * @author Haikah Ghoghari
+ */
 public class Bag {
 
 	private int width;
@@ -8,6 +13,16 @@ public class Bag {
 	private double weight;
 	private double excessCharge = 0;
 	
+	/**
+	 * Bag creating constructor
+	 * 
+	 * @param width
+	 * @param length
+	 * @param height
+	 * @param weight
+	 * 
+	 */
+	
 	public Bag(int width, int length, int height, double weight) {
 		this.width = width;
 		this.length = length;
@@ -15,26 +30,69 @@ public class Bag {
 		this.weight = weight;
 	}
 
+
+	/**
+	 * get method to return width
+	 * 
+	 * @return width
+	 */
+	
 	public int getWidth() {
 		return width;
 	}
 
+
+	/**
+	 * get method to return length
+	 * 
+	 * @return length
+	 */
+	
 	public int getLength() {
 		return length;
 	}
 
+
+	/**
+	 * get method to return height
+	 * 
+	 * @return height
+	 */
+	
 	public int getHeight() {
 		return height;
 	}
+	
+
+	/**
+	 * get method to return weight
+	 * 
+	 * @return weight
+	 */
+	
 
 	public double getWeight() {
 		return weight;
 	}
 	
+
+	/**
+	 * get method to return volume
+	 *  volume is calculated by multiplying length * width * height
+	 * @return volume
+	 */
+	
 	public double getVolume() {
 		double volume = length * width * height;
 		return volume;
 	}
+	
+
+	/**
+	 *Creating a method to set excess charge for baggage that weight more than allowed weight
+	 * Excess weight is calculated by subtracting allowed weight from total weight
+	 * Excess charge is then calculated by multiplying excess weight with set excess charge
+	 */
 
 	public void setExcessCharge(double allowedWeight, double excessCharge) {
 		double excessWeight = weight - allowedWeight;
@@ -42,6 +100,13 @@ public class Bag {
 			this.excessCharge = excessWeight * excessCharge;
 		}
 	}
+	
+
+	/**
+	 * get method to return excess charge
+	 * 
+	 * @return excessCharge
+	 */
 	
 	public double getExcessCharge() {
 		return excessCharge;
