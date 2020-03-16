429ab31e4e051bc8a47b3bfba5bfb6e7148d2932
Amy McFarland <am464@hw.ac.uk>
2020-03-16 12:10:40 +0000

    updated flightinformation

--- src/checkInGUI/FlightInformation.java
+++ src/checkInGUI/FlightInformation.java
@@ -1,7 +1,9 @@
 package checkInGUI;
 
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -9,18 +11,20 @@ class FlightInformation extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
 	
-	public FlightInformation(String flightCode, String destination) {
+	public FlightInformation(String flightCode, String destination, String status) {
 		
-		this.setLayout(new GridLayout(2, 1));
+		this.setLayout(new BorderLayout());
 		JLabel flightCodeLbl = new JLabel();
 		flightCodeLbl.setText(flightCode);
-		
+		JLabel flightstatus = new JLabel();
+		flightCodeLbl.setText(status);
 
 		JLabel destinationLbl = new JLabel();
 		destinationLbl.setText(destination);
 		
 		this.add(flightCodeLbl);
 		this.add(destinationLbl);
+		this.add(flightstatus);
 
 		this.setVisible(true);
 	}
