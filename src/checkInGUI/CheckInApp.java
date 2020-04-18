7711a01dd1e31b2af14216a6e837b8ad948eba1a
jamie <jamiefhill@gmail.com>
2020-04-18 17:05:59 +0100

    Refactored check in app / main thread

--- src/checkInGUI/CheckInApp.java
+++ src/checkInGUI/CheckInApp.java
@@ -14,19 +14,30 @@ import model.CheckIn;
 public class CheckInApp {
    
     /**
-	 * Method to run GUI 
+	 * Method to run GUI and the whole application.
 	 * @param arg
      * @throws Exception 
      * @throws FlightException 
 	 */
     public static void main (String arg[]) throws FlightException, Exception  {
 		try {
+			/**
+			 * Create the model that our system uses.
+			 */
 			CheckIn checkInDesk = new CheckIn();
+			/**
+			 * Create the view that our system uses, pass the model as a parameter
+			 * so that the view can observe it.
+			 */
 			CheckInSimulation checkInView = new CheckInSimulation(checkInDesk);
+			/**
+			 * Connect view and model in our controller.
+			 */
 			new CheckInController(checkInView, checkInDesk);
-		} catch(BookingException e) {
-			JOptionPane.showMessageDialog(null, e.getMessage());
-		} catch(CheckInIOException e) {
+		} catch(CheckInIOException | BookingException e) {
+			/**
+			 * Catch any Check in or Booking exceptions, display message then terminate application.
+			 */
 			JOptionPane.showMessageDialog(null, e.getMessage());
 		}
     }
