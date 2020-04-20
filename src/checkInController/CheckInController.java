d1373942ea8032a5bf151921daf19ad88a0fa294
jamie <jamiefhill@gmail.com>
2020-04-20 15:14:24 +0100

    Refactor and commented check in controller.

--- src/checkInController/CheckInController.java
+++ src/checkInController/CheckInController.java
@@ -1,34 +1,80 @@
 package checkInController;
 
+/**
+ * Import user interface packages to interact with the view.
+ */
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JComboBox;
 
+/**
+ * Import our view and model packages.
+ */
 import checkInGUI.CheckInSimulation;
 import checkInModel.CheckIn;
 
+/**
+ * CheckInController
+ * Connects the view and the model together
+ * so that they can interact.
+ * @author jamiehill
+ *
+ */
 public class CheckInController {
 
+	/**
+	 * The view component.
+	 */
 	private CheckInSimulation checkInView;
 
+	/**
+	 * The model component.
+	 */
 	private CheckIn checkInModel;
+	
+	/**
+	 * Thread that holds the execution of
+	 * our simulation.
+	 */
 	private Thread checkInSimulation;
 
+	/**
+	 * CheckInController
+	 * Constructor that creates the controller object
+	 * @param view
+	 * @param model
+	 */
 	public CheckInController(CheckInSimulation view, CheckIn model) {
-
+		/**
+		 * Store the view and model.
+		 */
 		checkInView = view;
 		checkInModel = model;
-		// specify the listener for the view
+		/**
+		 * Load action listeners into the view so that
+		 * user inputs can be sent into the model.
+		 */
 		checkInView.setStartSimulationAction(new CheckInSimulationStartAction());
-		//view.setCheckInDeskAction(new CheckInDeskStatusActionSetter());
 		checkInView.setSimSpeedAction(new SpeedSimulatorActionSetter());
 		checkInView.setWindowClosingAction(new CheckInDeskCloseWindowAction());
-		//checkInView.update(checkInModel);
-		//checkInModel.registerObserver(checkInView);
 	}
 	
+	/**
+	 * CheckInSimulationStartAction
+	 * The action listener that allows the simulation
+	 * to be started or stopped when a user clicks a
+	 * button on the view.
+	 * @author jamiehill
+	 *
+	 */
 	public class CheckInSimulationStartAction implements ActionListener {
 		
+		/**
+		 * actionPerformed
+		 * Allows the simulation to be started or
+		 * stopped when a user clicks a button on
+		 * the view.
+		 */
 		@SuppressWarnings("deprecation")
 		@Override
 		public void actionPerformed(ActionEvent e) {
@@ -42,19 +88,25 @@ public class CheckInController {
 		}
 	}
 
-	/*public class CheckInDeskStatusActionSetter implements ActionListener {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			JButton deskStatusButton = (JButton) e.getSource();
-			String name = deskStatusButton.getName();
-			checkInModel.getCheckInDesk(Integer.parseInt(name)).toggleclosestatus();
-			checkInModel.notifyObservers();
-		}
-
-	}*/
-
+	/**
+	 * CheckInDeskCloseAction
+	 * Action Listener that allows for the simulation
+	 * to be stopped when the user clicks the X to
+	 * close the window. Also writes the log to file
+	 * on exit.
+	 * @author jamiehill
+	 *
+	 */
 	public class CheckInDeskCloseWindowAction implements ActionListener {
+		
+		/**
+		 * actionPerformed
+		 * Triggered when the user clicks the X on 
+		 * the window and ensures that the simulation
+		 * stops, cleaning up any running threads. Also
+		 * writes the log to file.
+		 */
+		@SuppressWarnings("deprecation")
 		public void actionPerformed(ActionEvent e) {
 			if(checkInModel.getSimulationRunning() == true) {
 				checkInSimulation.stop();
@@ -63,8 +115,23 @@ public class CheckInController {
 		}
 	}
 
+	/**
+	 * SpeedSimulatorActionSetter
+	 * Sets the speed of the simulation in the model
+	 * based on a user's selection from a drop down
+	 * list.
+	 * @author jamiehill
+	 *
+	 */
 	public class SpeedSimulatorActionSetter implements ActionListener {
 
+		/**
+		 * actionPerformed
+		 * Sets the speed of the simulation in the
+		 * model based on a user's selection from
+		 * a drop down box.
+		 */
+		@SuppressWarnings("unchecked")
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			JComboBox<String> speedList = (JComboBox<String>) e.getSource();
