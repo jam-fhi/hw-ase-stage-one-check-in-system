54ab8fbee75683fe4257a20ab70c8040dcfc48ea
jamie <jamiefhill@gmail.com>
2020-04-20 14:59:14 +0100

    Removed try/catch no longer needed

--- src/checkInGUI/CheckInApp.java
+++ src/checkInGUI/CheckInApp.java
@@ -1,18 +1,11 @@
 package checkInGUI;
 
 /**
- * Import packages to display options.
+ * Import Model and Controller packages.
  */
-import javax.swing.JOptionPane;
-
-import checkInModel.BookingException;
+import checkInController.CheckInController;
 import checkInModel.CheckIn;
-import checkInModel.CheckInIOException;
 import checkInModel.FlightException;
-/**
- * Import our controller.
- */
-import controller.CheckInController;
 
 /**
  * CheckInApp
@@ -27,25 +20,18 @@ public class CheckInApp {
      * @throws FlightException 
 	 */
     public static void main (String arg[]) throws FlightException, Exception  {
-		try {
-			/**
-			 * Create the model that our system uses.
-			 */
-			CheckIn checkInDesk = new CheckIn();
-			/**
-			 * Create the view that our system uses, pass the model as a parameter
-			 * so that the view can observe it.
-			 */
-			CheckInSimulation checkInView = new CheckInSimulation(checkInDesk);
-			/**
-			 * Connect view and model in our controller.
-			 */
-			new CheckInController(checkInView, checkInDesk);
-		} catch(CheckInIOException | BookingException e) {
-			/**
-			 * Catch any Check in or Booking exceptions, display message then terminate application.
-			 */
-			JOptionPane.showMessageDialog(null, e.getMessage());
-		}
+		/**
+		 * Create the model that our system uses.
+		 */
+		CheckIn checkInDesk = new CheckIn();
+		/**
+		 * Create the view that our system uses, pass the model as a parameter
+		 * so that the view can observe it.
+		 */
+		CheckInSimulation checkInView = new CheckInSimulation(checkInDesk);
+		/**
+		 * Connect view and model in our controller.
+		 */
+		new CheckInController(checkInView, checkInDesk);
     }
 }
